cef95b57da24305bdc3d07be1c89bed34e758b76
Pierre-antoine Comby <comby@crans.org>
2019-07-08 15:29:03 +0200

    add lazy_text

--- note/models.py
+++ note/models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 
+from django.utils.translation import gettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
