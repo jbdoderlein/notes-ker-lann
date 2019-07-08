9db72d4f346a1fc07c8e14ca2ee2e106de62e201
Pierre-antoine Comby <comby@crans.org>
2019-07-08 16:11:10 +0200

    register models

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import NoteClub,NoteSpec,NoteUser
+from .models import Alias
 # Register your models here.
+admin.site.register(NoteClub)
+admin.site.register(NoteSpec)
+admin.site.register(NoteUser)
+admin.site.register(Alias)
