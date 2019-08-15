7a7cc2bfeff2464679c2b804032a548f3a6fc57d
Pierre-antoine Comby <comby@crans.org>
2019-08-15 23:12:27 +0200

    minor fixes

--- apps/member/tables.py
+++ apps/member/tables.py
@@ -3,7 +3,6 @@
 import django_tables2 as tables
 from .models import Club
 
-
 class ClubTable(tables.Table):
     class Meta:
         attrs = {'class':'table table-bordered table-condensed table-striped table-hover'}
