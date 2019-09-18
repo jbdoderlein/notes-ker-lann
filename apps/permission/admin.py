d826dc9d2076fcc032eafaf9cbe80f38a5c8f2f1
Benjamin Graillot <graillot@crans.org>
2019-09-18 16:40:21 +0200

    [permission] Permission admin view

--- apps/permission/admin.py
+++ apps/permission/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Permission
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    """
+    Admin customisation for Permission
+    """
+    list_display = ('type', 'model', 'field', 'permission')
