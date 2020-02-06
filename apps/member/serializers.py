8e2b0688b56ace904726ac54ebff4394116693dc
Yohann D'ANELLO <yohann.danello@gmail.com>
2020-02-06 23:44:59 +0100

    Add member models in API

--- apps/member/serializers.py
+++ apps/member/serializers.py
@@ -1,3 +1,7 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (C) 2018-2020 by BDE ENS Paris-Saclay
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from .models import Profile, Club, Role, Membership
 from rest_framework import serializers, viewsets
 
