c4198a64afa3b15e11b9169a26a3afc24c463d3a
Pierre-antoine Comby <comby@crans.org>
2020-03-03 14:45:50 +0100

    add button for profile pic view

--- apps/member/urls.py
+++ apps/member/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('user/', views.UserListView.as_view(), name="user_list"),
     path('user/<int:pk>', views.UserDetailView.as_view(), name="user_detail"),
     path('user/<int:pk>/update', views.UserUpdateView.as_view(), name="user_update_profile"),
+    path('user/<int:pk>/update_pic', views.UserUpdateView.as_view(), name="user_update_pic"),
     path('user/<int:pk>/aliases', views.AliasView.as_view(), name="user_alias"),
     path('user/aliases/delete/<int:pk>', views.DeleteAliasView.as_view(), name="user_alias_delete"),
     path('manage-auth-token/', views.ManageAuthTokens.as_view(), name='auth_token'),
