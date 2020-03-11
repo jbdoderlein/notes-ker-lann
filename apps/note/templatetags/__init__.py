523e27694a5589a6b6f9fafa5b249efb5d063522
Yohann D'ANELLO <yohann.danello@gmail.com>
2020-03-11 15:14:51 +0100

    Remove useless code

--- apps/note/templatetags/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2018-2020 by BDE ENS Paris-Saclay
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-from . import getenv
-from . import pretty_money
-
-print(getenv, pretty_money, file=None)  # Useless, but tox is happy
