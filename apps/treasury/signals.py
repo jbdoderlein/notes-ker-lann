038ddc2ab87ab384d85948a7fafa1b403bafab47
Pierre-antoine Comby <comby@crans.org>
2020-04-18 15:59:59 +0200

    remittance need preciser check (bug during import)

--- apps/treasury/signals.py
+++ apps/treasury/signals.py
@@ -8,5 +8,10 @@ def save_special_transaction(instance, created, **kwargs):
     """
     When a special transaction is created, we create its linked proxy
     """
-    if created and RemittanceType.objects.filter(note=instance.source).exists():
-        SpecialTransactionProxy.objects.create(transaction=instance, remittance=None).save()
+
+    if instance.is_credit():
+        if created and RemittanceType.objects.filter(note=instance.source).exists():
+            SpecialTransactionProxy.objects.create(transaction=instance, remittance=None).save()
+    else:
+        if created and RemittanceType.objects.filter(note=instance.destination).exists():
+            SpecialTransactionProxy.objects.create(transaction=instance, remittance=None).save()
