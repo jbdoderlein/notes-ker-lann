750bdcb2c55ee239e7270b6ebc8c8784ca58a52a
Yohann D'ANELLO <yohann.danello@gmail.com>
2020-07-29 12:25:53 +0200

    Treasurers can of course click on buttons. Fix PATCH requests on the API

--- apps/permission/permissions.py
+++ apps/permission/permissions.py
@@ -19,8 +19,8 @@ class StrongDjangoObjectPermissions(DjangoObjectPermissions):
         'OPTIONS': [],
         'HEAD': [],
         'POST': ['%(app_label)s.add_%(model_name)s'],
-        'PUT': ['%(app_label)s.change_%(model_name)s'],
-        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'PUT': [],      # ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': [],    # ['%(app_label)s.change_%(model_name)s'],
         'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
 
