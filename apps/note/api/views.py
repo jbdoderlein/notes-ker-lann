9008baad3a40d20d37fc89ec763f36365aa3efb8
Yohann D'ANELLO <yohann.danello@gmail.com>
2020-07-31 17:07:14 +0200

    Better alias research, closes #51

--- apps/note/api/views.py
+++ apps/note/api/views.py
@@ -106,7 +106,10 @@ class ConsumerViewSet(ReadOnlyProtectedModelViewSet):
         queryset = super().get_queryset()
 
         alias = self.request.query_params.get("alias", ".*")
-        queryset = queryset.filter(normalized_name__iregex="^" + Alias.normalize(alias))\
+        queryset = queryset.filter(
+            Q(name__regex="^" + alias)
+            | Q(normalized_name__regex="^" + Alias.normalize(alias))
+            | Q(normalized_name__regex="^" + alias.lower()))\
             .order_by('name').prefetch_related('note')
 
         return queryset
