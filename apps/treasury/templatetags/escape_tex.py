a4702fca86c5b1573b2bc70bd7a9c3ecaccabea7
Yohann D'ANELLO <yohann.danello@gmail.com>
2020-08-06 14:19:51 +0200

    Escape special TeX characters

---
+++ apps/treasury/templatetags/escape_tex.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2018-2020 by BDE ENS Paris-Saclay
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from django import template
+
+
+def do_latex_escape(value):
+    return (
+        value.replace("&", "\\&")
+        .replace("$", "\\$")
+        .replace("%", "\\%")
+        .replace("#", "\\#")
+        .replace("_", "\\_")
+        .replace("{", "\\{")
+        .replace("}", "\\}")
+    )
+
+
+register = template.Library()
+register.filter("escape_tex", do_latex_escape)
