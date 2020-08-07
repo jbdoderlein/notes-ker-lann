9751a5ad92f674cb796d4be0ae159a9a6cc0fa6d
Yohann D'ANELLO <yohann.danello@gmail.com>
2020-08-07 12:55:07 +0200

    :bug: Fix pagination in transaction page

--- apps/note/views.py
+++ apps/note/views.py
@@ -214,7 +214,7 @@ class TransactionSearchView(ProtectQuerysetMixin, LoginRequiredMixin, DetailView
             transactions = transactions.filter(created_at__lte=data["created_before"])
 
         table = HistoryTable(transactions)
-        table.paginate(per_page=100)
+        table.paginate(per_page=100, page=self.request.GET.get("page", 1))
         context["table"] = table
 
         return context
