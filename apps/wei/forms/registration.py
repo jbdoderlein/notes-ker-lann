5e75a56edadacbe1fbd95cac315b4770c826faee
Alexandre Iooss <erdnaxe@crans.org>
2020-08-09 19:38:13 +0200

    missing whitespace after ':' in apps/wei/forms/registration.py

--- apps/wei/forms/registration.py
+++ apps/wei/forms/registration.py
@@ -39,8 +39,8 @@ class WEIRegistrationForm(forms.ModelForm):
                     'placeholder': 'Nom ...',
                 },
             ),
-            "birth_date": DatePickerInput(options={'defaultDate':'2000-01-01',
-                                                   'minDate':'1900-01-01',
+            "birth_date": DatePickerInput(options={'defaultDate': '2000-01-01',
+                                                   'minDate': '1900-01-01',
                                                    'maxDate': '2100-01-01'}),
         }
 
