d73f7c31a1cff04842f4539edd2d4cc866aee501
Alexandre Iooss <erdnaxe@crans.org>
2020-08-09 19:36:11 +0200

    Define BASE_DIR in development.py

--- note_kfet/settings/development.py
+++ note_kfet/settings/development.py
@@ -12,6 +12,9 @@
 
 import os
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 if os.getenv("DJANGO_DEV_STORE_METHOD", "sqlite") != "postgresql":
     # Use an SQLite database
     DATABASES = {
