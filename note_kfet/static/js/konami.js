76aacaf048f3d12c8d34587405ae594c89e96c80
Alexandre Iooss <erdnaxe@crans.org>
2020-09-03 11:47:17 +0200

    Definitively more usable

--- note_kfet/static/js/konami.js
+++ note_kfet/static/js/konami.js
@@ -9,21 +9,28 @@ const KONAMI_CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function afterKonami() {
     // Load Rythm.js
     var rythmScript = document.createElement('script')
-    rythmScript.setAttribute('src','https://unpkg.com/rythm.js@2.2.5/rythm.min.js')
+    rythmScript.setAttribute('src','//unpkg.com/rythm.js@2.2.5/rythm.min.js')
     document.head.appendChild(rythmScript)
 
     rythmScript.addEventListener('load', function() {
-        // This media source need to be accessible with a cross-origin header
-        const audioElement = new Audio('https://okazari.github.io/Rythm.js/samples/rythmC.mp3')
-        audioElement.crossOrigin = 'anonymous'
-        audioElement.play();
+        // Ker-Lyon audio courtesy of @adalan, ker-lyon.fr
+        const audioElement = new Audio('/static/song/konami.ogg')
+        audioElement.loop = true
+        audioElement.play()
 
         const rythm = new Rythm()
         rythm.connectExternalAudioElement(audioElement)
-        rythm.addRythm('card', 'pulse', 0, 10)
-        rythm.addRythm('nav-link', 'color', 0, 10, {
-           from: [0,0,255],
-           to:[255,0,255]
+        rythm.addRythm('card', 'pulse', 50, 50, {
+            min: 1,
+            max: 1.1
+        })
+        rythm.addRythm('d-flex', 'color', 50, 50, {
+           from: [64,64,64],
+           to:[128,64,128]
+        })
+        rythm.addRythm('nav-link', 'jump', 150, 50, {
+              min: 0,
+              max: 10
         })
         rythm.start()
     });
