72806f0aceb08e61600226a63583e440f94619fe
Yohann D'ANELLO <ynerant@crans.org>
2021-03-09 10:57:35 +0100

    Add profile and membership information to OAuth views
    Signed-off-by: Yohann D'ANELLO <ynerant@crans.org>

--- apps/api/views.py
+++ apps/api/views.py
@@ -4,11 +4,14 @@
 from django.contrib.auth.models import User
 from rest_framework.generics import RetrieveAPIView
 
-from .serializers import UserSerializer
+from .serializers import OAuthSerializer
 
 
 class UserInformationView(RetrieveAPIView):
-    serializer_class = UserSerializer
+    """
+    These fields are give to OAuth authenticators.
+    """
+    serializer_class = OAuthSerializer
 
     def get_queryset(self):
         return User.objects.filter(pk=self.request.user.pk)
