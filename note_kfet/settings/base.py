76d1784aeabfa65f96f429593f0ea7ca0d3092c6
Yohann D'ANELLO <ynerant@crans.org>
2021-03-09 09:44:25 +0100

    Add OAuth2 authentication for Django Rest Framework
    Signed-off-by: Yohann D'ANELLO <ynerant@crans.org>

--- note_kfet/settings/base.py
+++ note_kfet/settings/base.py
@@ -239,6 +239,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
